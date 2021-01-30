d469cd925ced802ed4ea9ef0301d24185806389c
O. Kaya <ogulcankaya117@gmail.com>
2021-01-31 00:55:39 +0300

    aaaaaaa

--- cat.py
+++ cat.py
@@ -5,31 +5,21 @@ import os
 x = 1400
 cycle = 0
 check = 1
-idle_num =[1,2,3,4,5,6,7,8,9]
-sleep_num = [10,11,12,13,15]
-#walk_left = [6,7]
-#walk_right = [8,9]
-event_number = random.randrange(1,3,1)
+idle_num =[1,2]
+sleep_num = [3,4,5,6,7]
+event_number = random.randrange(1,7,1)
 impath = os.path.dirname(__file__)
-print('doing ur mum')
+print('running lol')
 #transfer random no. to event
 def event(cycle,check,event_number,x):
     if event_number in idle_num:
         check = 0
         print('idle')
         window.after(400,update,cycle,check,event_number,x) #no. 1,2,3,4 = idle
-    elif event_number == 5:
-        check = 2
-        print('from idle to sleep')
-        window.after(100,update,cycle,check,event_number,x) #no. 5 = idle to sleep
-    elif event_number in sleep_num:
+    else:
         check  = 2
         print('sleep')
         window.after(1000,update,cycle,check,event_number,x)#no. 10,11,12,13,15 = sleep
-    elif event_number == 14:
-        check = 0
-        print('from sleep to idle')
-        window.after(100,update,cycle,check,event_number,x)#no. 15 = sleep to idle
 #making gif work 
 def gif_work(cycle,frames,event_number,first_num,last_num):
     if cycle < len(frames) -1:
@@ -42,26 +32,16 @@ def update(cycle,check,event_number,x):
     #idle
     if check ==0:
         frame = idle[cycle]
-        cycle ,event_number = gif_work(cycle,idle,event_number,1,9)
-  
-    #idle to sleep
-#    elif check ==1:
-#        frame = idle_to_sleep[cycle]
-#        cycle ,event_number = gif_work(cycle,idle_to_sleep,event_number,10,10)
+        cycle ,event_number = gif_work(cycle,idle,event_number,1,7)
 #sleep
     elif check == 2:
         frame = sleep[cycle]
-        cycle ,event_number = gif_work(cycle,sleep,event_number,10,15)
-#sleep to idle
-#    elif check ==3:
-#        frame = sleep_to_idle[cycle]
-#        cycle ,event_number = gif_work(cycle,sleep_to_idle,event_number,1,1)
+        cycle ,event_number = gif_work(cycle,sleep,event_number,1,7)
+
 window = tk.Tk()
 #call buddy's action gif
 idle = [tk.PhotoImage(file=impath+'\\idle.gif' ,format = 'gif -index %i' %(i)) for i in range(10)]#idle gif
-#idle_to_sleep = [tk.PhotoImage(file=impath+'\\idle_to_sleep.gif',format = 'gif -index %i' %(i)) for i in range(8)]#idle to sleep gif
 sleep = [tk.PhotoImage(file=impath+'\\sleep.gif',format = 'gif -index %i' %(i)) for i in range(7)]#sleep gif
-#sleep_to_idle = [tk.PhotoImage(file=impath+'\\sleep_to_idle.gif',format = 'gif -index %i' %(i)) for i in range(8)]#sleep to idle gif
 
 #window configuration
 window.config(highlightbackground='white')
