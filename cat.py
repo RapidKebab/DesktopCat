9643830b1719b2b70dae8198b9c2250d7be68a0a
Vasya <vasya@petrov.com>
2021-01-30 21:22:56 -0500

    added functions to display all gifs

--- cat.py
+++ cat.py
@@ -3,6 +3,7 @@ import pyautogui
 import random
 import tkinter as tk
 import os
+import sys
 x = 1400
 cycle = 0
 check = 1
@@ -10,25 +11,40 @@ idle_num =[1,2]
 sleep_num = [3,4,5,6,7]
 event_number = random.randrange(1,7,1)
 impath = os.path.dirname(__file__)
-print('running lol')
 
-
+#window configuration
 t = tk.Tk()
 t.title("Mishookoo")
-frame = tk.Frame(t, highlightthickness=0,borderwidth=0)
+frame = tk.Frame(t, highlightthickness=-2,borderwidth=-2)
 frame.pack()
 t.overrideredirect(True)
-screen_width = t.winfo_screenwidth()-50-240
-screen_height = t.winfo_screenheight()-10-240
+screen_width = t.winfo_screenwidth()-240
+screen_height = t.winfo_screenheight()-32-240
 screen_resolution = '+'+str(screen_width)+'+'+str(screen_height)
 t.geometry(screen_resolution)
-t.wm_attributes("-transparentcolor", "red")
-
-
+t.wm_attributes("-transparentcolor", "red",'-topmost', True)
+t.lift()
 canvas = tk.Canvas(frame,width=240,height=240,bd=-2)
-# canvas.pack(expand=True, fill="both")
-# gif1=tk.PhotoImage(file='./redcat.gif', format="gif -index 2")
-# canvas.create_image(0,0, image=gif1, anchor = tk.NW )
+
+def do_popup(event):
+    try:
+        m.tk_popup(event.x_root, event.y_root)
+    finally:
+        m.grab_release()
+
+label1_text=tk.StringVar()
+label1_text.set("Mishookoo")
+label1=tk.Label(t, textvariable=label1_text)
+label1.pack()
+# attach popup to frame
+label1.bind("<Button-3>", do_popup)
+
+L = tk.Label(t, text="Right-click to display menu", width=40, height=20)
+# L.pack()
+m = tk.Menu(t, tearoff=0)
+m.add_command(label="Exit", command=exit)
+frame.bind("<Button-3>", do_popup) 
+
 
 frameCnt = 12
 frames = [tk.PhotoImage(file='./sleeping12.gif',format = 'gif -index %i' %(i)).subsample(2,2) for i in range(12)]
@@ -41,10 +57,55 @@ def update(ind):
         ind = 0
     label.configure(image=frame)
     t.after(250, update, ind)
+    
 label = tk.Label(t,borderwidth=0)
 label.pack()
-t.after(0, update, 0)
 
+frameCntSleep = 12
+framesSleep = [tk.PhotoImage(file='./sleeping12.gif',format = 'gif -index %i' %(i)).subsample(2,2) for i in range(12)]
+
+def showSleep(ind):
+    frame = framesSleep[ind]
+    ind += 1
+    if ind == frameCntSleep:
+        ind = 0
+    label.configure(image=frame)
+    t.after(250, showSleep, ind)
+
+frameCntIdle = 32
+framesIdle = [tk.PhotoImage(file='./idle32.gif',format = 'gif -index %i' %(i)).subsample(2,2) for i in range(12)]
+
+def showIdle(ind):
+    frame = framesIdle[ind]
+    ind += 1
+    if ind == frameCntIdle:
+        ind = 0
+    label.configure(image=frame)
+    t.after(250, showIdle, ind)
+
+frameCntI2S = 24
+framesIdle2Sleep = [tk.PhotoImage(file='./transitionIdle2Sleep24.gif',format = 'gif -index %i' %(i)).subsample(2,2) for i in range(12)]
+
+def showIdle2Sleep(ind):
+    frame = framesIdle2Sleep[ind]
+    ind += 1
+    if ind == frameCntI2S:
+        ind = 0
+    label.configure(image=frame)
+    t.after(250, showIdle2Sleep, ind)
+
+frameCntS2I = 24
+framesSleep2Idle = [tk.PhotoImage(file='./transitionSleep2Idle24.gif',format = 'gif -index %i' %(i)).subsample(2,2) for i in range(12)]
+
+def showSleep2Idle(ind):
+    frame = framesSleep2Idle[ind]
+    ind += 1
+    if ind == frameCntS2I:
+        ind = 0
+    label.configure(image=frame)
+    t.after(250, showSleep, ind)
+
+t.after(0, update, 0)
 
 tk.mainloop()
 
